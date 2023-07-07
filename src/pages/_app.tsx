ffda292d84cbe98b4bcee49b274f91d7701c11dc
Abiola Olamilekan <abiolawalter2015@gmail.com>
2023-07-07 14:07:35 +0100

    feat: implement tailwind css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,38 @@
-import { initializeApp } from 'firebase/app';
-import NextNProgress from 'nextjs-progressbar';
-import { Provider } from 'react-redux';
+// import { initializeApp } from 'firebase/app';
+// import NextNProgress from 'nextjs-progressbar';
+// import { Provider } from 'react-redux';
 
 //--INTERNAL IMPORTS
+// import type { AppProps } from 'next/app';
+// import { config } from '@/src/config/index';
+// import { store } from '@/store';
+
+// import '../styles/globals.css';
+
+// initializeApp(config.firebaseConfig);
+
+// export default function App({ Component, pageProps }: AppProps) {
+//   return (
+//     <Provider store={store}>
+//       <NextNProgress />
+//       <Component {...pageProps} />
+//     </Provider>
+//   );
+// }
+
 import type { AppProps } from 'next/app';
-import { config } from '@/src/config/index';
-import { store } from '@/store';
-
 import '../styles/globals.css';
+import { NextPageWithLayout } from './page';
 
-initializeApp(config.firebaseConfig);
-
-export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <Provider store={store}>
-      <NextNProgress />
-      <Component {...pageProps} />
-    </Provider>
-  );
+interface AppPropsWithLayout extends AppProps {
+  Component: NextPageWithLayout;
 }
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  // Use the layout defined at the page level, if available
+  const getLayout = Component.getLayout || ((page) => page);
+
+  return getLayout(<Component {...pageProps} />);
+}
+
+export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,63 +1,89 @@
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
+// import { getAuth, onAuthStateChanged } from 'firebase/auth';
+// import { useEffect } from 'react';
+// import { useDispatch } from 'react-redux';
 
 //--INTERNAL IMPORTS
-import MainPageLayout from '@/src/layouts/MainPageLayout';
-import HomeHero from '@/src/components/homepage/HomeHero';
-import NewProduct from '@/src/components/homepage/NewProduct';
-import TrendingProducts from '@/src/components/homepage/TrendingProduct';
-import { Banner, ReverseBanner } from '@/src/components/common/Banner';
+// import { Banner, ReverseBanner } from '@/src/components/common/Banner';
+// import HomeHero from '@/src/components/homepage/HomeHero';
+// import NewProduct from '@/src/components/homepage/NewProduct';
+// import TrendingProducts from '@/src/components/homepage/TrendingProduct';
+// import MainPageLayout from '@/src/layouts/MainPageLayout';
 
-import { loggedIn } from '@/src/redux/userSlice';
-import { loginFn } from '@/src/services/queries/auth';
+// import { loggedIn } from '@/src/redux/userSlice';
+// import { loginFn } from '@/src/services/queries/auth';
 
 //--IMPORT CONSTANTS
-import { banner1, banner2 } from '@/public/img/banner';
+// import { banner1, banner2 } from '@/public/img/banner';
 
-export default function Home() {
-  const auth = getAuth();
-  const dispatch = useDispatch();
+// export default function Home() {
+//   const auth = getAuth();
+//   const dispatch = useDispatch();
 
-  useEffect(() => {
-    const AuthCheck = onAuthStateChanged(auth, (user: any) => {
-      if (user) {
-        let authToken = user.accessToken;
-        loginFn(authToken)
-          .then((res) => {
-            dispatch(
-              loggedIn({
-                name: res.data.name,
-                email: res.data.email,
-                role: res.data.role,
-                token: authToken,
-                _id: res.data._id,
-              })
-            );
-          })
-          .catch((err) => {
-            console.log('backend_error->', err);
-          });
-      }
-    });
-    return () => AuthCheck();
-  }, [auth, dispatch]);
+//   useEffect(() => {
+//     const AuthCheck = onAuthStateChanged(auth, (user: any) => {
+//       if (user) {
+//         let authToken = user.accessToken;
+//         loginFn(authToken)
+//           .then((res) => {
+//             dispatch(
+//               loggedIn({
+//                 name: res.data.name,
+//                 email: res.data.email,
+//                 role: res.data.role,
+//                 token: authToken,
+//                 _id: res.data._id,
+//               })
+//             );
+//           })
+//           .catch((err) => {
+//             console.log('backend_error->', err);
+//           });
+//       }
+//     });
+//     return () => AuthCheck();
+//   }, [auth, dispatch]);
 
+//   return (
+//     <MainPageLayout>
+//       <HomeHero />
+//       <NewProduct />
+//       <Banner
+//         img={banner1}
+//         title="Creative harmonious living"
+//         text=" RAOUF Products are all made to standard sizes so that you can mix and match them freely."
+//       />
+//       <TrendingProducts />
+//       <ReverseBanner
+//         img={banner2}
+//         title="Creative harmonious living"
+//         text=" RAOUF Products are all made to standard sizes so that you can mix and match them freely."
+//       />
+//     </MainPageLayout>
+//   );
+// }
+
+import { mockSidebarLayoutProps } from '@/src/components/layouts/sidebar/SidebarLayout.mocks';
+import PrimaryLayout from '../components/layouts/primary/PrimaryLayout';
+import SidebarLayout from '../components/layouts/sidebar/SidebarLayout';
+import { NextPageWithLayout } from './page';
+
+const Home: NextPageWithLayout = () => {
   return (
-    <MainPageLayout>
-      <HomeHero />
-      <NewProduct />
-      <Banner
-        img={banner1}
-        title="Creative harmonious living"
-        text=" RAOUF Products are all made to standard sizes so that you can mix and match them freely."
-      />
-      <TrendingProducts />
-      <ReverseBanner
-        img={banner2}
-        title="Creative harmonious living"
-        text=" RAOUF Products are all made to standard sizes so that you can mix and match them freely."
-      />
-    </MainPageLayout>
+    <section>
+      <h1>
+        Welcome to <a href="https://nextjs.org">Next.js!</a>
+      </h1>
+    </section>
   );
-}
+};
+
+export default Home;
+
+Home.getLayout = (page) => {
+  return (
+    <PrimaryLayout>
+      <SidebarLayout {...mockSidebarLayoutProps.base} />
+      {page}
+    </PrimaryLayout>
+  );
+};
